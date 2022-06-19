f2b2ab0188ffe8d888d454dc8bd26dc7b2d74b3f
thefvnday <fandy.09.ahmad@gmail.com>
2022-06-19 23:04:49 +0700

    learn react hook -> useState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  //single state
+  const [counter, setCounter] = useState(0);
+  const [foodies,setFoodies] = useState(['Mie Ayam', 'Bakso', 'Kupat Tahu']);
+
+  const addCounter = () => {
+    setCounter(counter + 1);
+  }
+
+  const minusCounter = () => {
+    setCounter(counter - 1);
+  }
+  console.log(foodies)
+  const addFoodie = () => {
+    setFoodies([...foodies,  'Ayam Goreng']);
+  }
+
+  //banyak multi state dalam satu variabel
+  // const [state, setState] = useState({
+  //   counter: 0,
+  //   foodies: ['Mie Ayam', 'Bakso', 'Kupat Tahu']
+  // })
+
+  // const addCounter= ()=>{
+  //   setState({counter:state.counter+1,foodies:['Kwetiau'] })
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Nilai Dari State Counter :</h1>
+        <h2>{counter}</h2>
+        <button onClick={addCounter}>Tambah</button>
+        <br />
+        <button onClick={minusCounter}>Kurang</button>
+
+        <div className='addString'>
+          <h1>Tampilkan String</h1>
+          <div>{foodies.map((foodie) => <p>{foodie}</p>)}</div>
+          <button onClick={addFoodie}>Tambah</button>
+        </div>
       </header>
     </div>
   );
